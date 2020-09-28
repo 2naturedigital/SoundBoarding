c1afb34a48e19dede26e89a4e38d546c159849af
James_Desktop <videovillain@gmail.com>
2020-09-28 15:56:33 +0900

    added code for grabbing file from users device files (but not working unfortunately)

--- app/src/main/java/edu/finalproject/cs50/soundboarding/MainActivity.java
+++ app/src/main/java/edu/finalproject/cs50/soundboarding/MainActivity.java
@@ -1,14 +1,24 @@
 package edu.finalproject.cs50.soundboarding;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Button button1;
@@ -18,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private Button button5;
     private MediaPlayer mp;
     public String btn_1_sound_file;
+    //public List<String> sounds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,20 +41,35 @@ public class MainActivity extends AppCompatActivity {
         button4 = findViewById(R.id.button4);
         button5 = findViewById(R.id.button5);
 
+        /*sounds = new ArrayList<>();
+        sounds.add("burn_out_the_computer");
+        sounds.add("i_like_what_you_got");
+        sounds.add("lick_lick_lick");
+        sounds.add("ooh_yeah_can_do");
+        sounds.add("portal_gun_louder");
+        sounds.add("squeeze_them");
+        sounds.add("what_did_you_do");
+        btn_1_sound_file = sounds.get(1);*/
+
         btn_1_sound_file = "burn_out_the_computer";
 
         button1.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                btn_1_sound_file = "portal_gun_louder";
+                //int soundID = (int)(Math.random() * 7 + 1);
+                //btn_1_sound_file = sounds.get(soundID);
+                btn_1_sound_file = "what_did_you_do";
                 Toast.makeText(view.getContext(), "Changed!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.setType("*/*");
+                startActivityForResult(intent, 1);
                 return false;
             }
         });
     }
 
     public void sound1(View v) {
-        int resID = this.getResources().getIdentifier(btn_1_sound_file,"raw", this.getPackageName());
+        int resID = getResources().getIdentifier(btn_1_sound_file,"raw", getPackageName());
         mp = MediaPlayer.create(this, resID);
         mp.start();
     }
@@ -64,10 +90,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void chooseSound(View v) {
-        Toast.makeText(v.getContext(), "Long Clicked!", Toast.LENGTH_LONG).show();
-        /*Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.setType("media/*");
-        startActivityForResult(intent, 1);*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // handle result
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            if (requestCode == 1) {
+                Uri uri = data.getData();
+                // try getting new sound via Uri
+                try {
+                    Log.i("cs50", "Uri: " + uri);
+                    mp.setDataSource(this, uri);
+                } catch (IOException e) {
+                    Log.i("cs50", "ERROR HIT", e);
+                    e.printStackTrace();
+                }
+                // try getting new sound via FileDescriptor
+                try {
+                    ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+                    FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                    // makes the image
+                    Log.i("cs50", "File Descriptor: " + fileDescriptor.toString());
+                    mp.setDataSource(fileDescriptor);
+                    parcelFileDescriptor.close();
+                } catch (IOException e) {
+                    Log.e("cs50", "Media not found", e);
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
