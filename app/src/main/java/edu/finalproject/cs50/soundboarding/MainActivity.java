68d0511cf1a1bdc34a9ead8e65159a45fd0e364a
James_Desktop <videovillain@gmail.com>
2020-09-28 13:01:50 +0900

    fixed file names and added on long click listener to first button with toast for testing

--- app/src/main/java/edu/finalproject/cs50/soundboarding/MainActivity.java
+++ app/src/main/java/edu/finalproject/cs50/soundboarding/MainActivity.java
@@ -3,7 +3,9 @@ package edu.finalproject.cs50.soundboarding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button button1;
@@ -22,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
         button3 = findViewById(R.id.button3);
         button4 = findViewById(R.id.button4);
         button5 = findViewById(R.id.button5);
+
+        button1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText(view.getContext(), "Long Clicked!", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
     }
 
 
