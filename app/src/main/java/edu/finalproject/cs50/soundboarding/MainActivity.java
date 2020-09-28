f3afa3adb9f7cab43624c8c3017faf167d7f5c4a
Richard_desktop <moorerichardplease@gmail.com>
2020-09-28 12:34:08 +0900

    UI update. added buttons into main UI & main java

--- app/src/main/java/edu/finalproject/cs50/soundboarding/MainActivity.java
+++ app/src/main/java/edu/finalproject/cs50/soundboarding/MainActivity.java
@@ -3,8 +3,14 @@ package edu.finalproject.cs50.soundboarding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
