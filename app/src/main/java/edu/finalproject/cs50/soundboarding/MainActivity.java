950436dbba3654d61bb562b9b6417b0769c585a7
Richard_desktop <moorerichardplease@gmail.com>
2020-09-28 12:44:52 +0900

    bind buttons to button views

--- app/src/main/java/edu/finalproject/cs50/soundboarding/MainActivity.java
+++ app/src/main/java/edu/finalproject/cs50/soundboarding/MainActivity.java
@@ -16,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
     }
+
+
 }
